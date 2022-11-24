bb613765150c072c1613403906b9d7858687c021
Angel Muñoz <angelmsanchez85@gmail.com>
2022-11-24 12:50:20 +0100

    change properties to web

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,30 +7,26 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
  */
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Angel Muñoz Sanchez`,
+    siteTitleAlt: `Angel Muñoz Sanchez - Portfolio`,
+    siteHeadline: `Angel Muñoz Sanchez`,
+    siteUrl: `https://angelmsanchez.com`,
+    siteDescription: `Portfolio Angel Muñoz Sanchez`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `angelmsanchez85`,
   },
   trailingSlash: `never`,
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      // See the theme's README for all available options
       options: {},
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Angel Muñoz Sanchez`,
+        short_name: `Angel Muñoz Sanchez`,
+        description: `Portfolio Angel Muñoz Sanchez`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
