044b48e484cd2c5a2bd29f97a02084de6c932a2d
Angel Muñoz <tok.amunoz@iberia.es>
2024-02-22 15:39:44 +0100

    change logos

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
 
-import { AmazonLogo } from "../assets/logos/AmazonLogo";
-import { DropboxLogo } from "../assets/logos/DropboxLogo";
-import { NetflixLogo } from "../assets/logos/NetflixLogo";
-import { SlackLogo } from "../assets/logos/SlackLogo";
-import { SpotifyLogo } from "../assets/logos/SpotifyLogo";
-import { StripeLogo } from "../assets/logos/StripeLogo";
+import { IonicLogo } from "../assets/logos/IonicLogo";
+import { ReactLogo } from "../assets/logos/ReactLogo";
+import { AngularLogo } from "../assets/logos/AngularLogo";
+import { HtmlLogo } from "../assets/logos/HtmlLogo";
+import { CssLogo } from "../assets/logos/CssLogo";
+import { TypescriptLogo } from "../assets/logos/TypescriptLogo";
 
 export const Brands = () => (
   <section className="py-12 sm:py-24 bg-customDarkBg1 w-full mt-16 mb-16">
@@ -19,34 +19,33 @@ export const Brands = () => (
         <div className="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center">
           <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
             <div className="flex flex-col">
-              <h2 className="mb-2  text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-white">
-                Trusted by brands
+              <h2 className="mb-2 text-3xl sm:text-5xl 2xl:text-5xl font-bold tracking-normal text-white">
+                Technologies which
               </h2>
-              <h2 className=" text-4xl  sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-customSecondary">
-                all over the world
+              <h2 className="text-3xl sm:text-5xl 2xl:text-5xl font-bold tracking-normal text-customSecondary">
+                I feel comfortable with
               </h2>
             </div>
           </div>
           <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
             <div className="flex flex-wrap -m-4">
               <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <AmazonLogo />
+                <ReactLogo />
               </div>
               <div className="w-1/2 sm:w-1/3 py-6 flex  justify-center">
-                <DropboxLogo />
+                <AngularLogo />
               </div>
               <div className="w-1/2 sm:w-1/3 py-6 flex  justify-center">
-                <NetflixLogo />
+                <CssLogo />
               </div>
               <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <StripeLogo />
+                <HtmlLogo />
               </div>
-
               <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <SpotifyLogo />
+                <IonicLogo />
               </div>
               <div className="w-1/2 sm:w-1/3  py-6 flex  justify-center">
-                <SlackLogo />
+                <TypescriptLogo />
               </div>
             </div>
           </div>
